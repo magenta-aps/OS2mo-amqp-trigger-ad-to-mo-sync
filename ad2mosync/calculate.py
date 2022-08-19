3dba45b426a19df96a2d028f56079b761d5ca1c0
Emil Madsen <emil@magenta.dk>
2022-08-19 11:45:01 +0200

    Rework address_type_uuid flow

--- ad2mosync/calculate.py
+++ ad2mosync/calculate.py
@@ -21,14 +21,12 @@ from .dataloaders import Dataloaders
 
 
 def construct_insert_addresses(
-    to_insert: set[UUID],
-    write_address_map: dict[UUID, str],
+    insert_address_map: dict[UUID, str],
     user_uuid: UUID,
 ) -> list[Address]:
     """Construct addresses to be inserted.
 
     Args:
-        to_insert: UUID of address_type for address to be inserted.
         write_address_map: Map of information to construct insert addresses with.
         user_uuid: The user to insert addresses for.
 
@@ -36,11 +34,11 @@ def construct_insert_addresses(
         List of Address objects to be inserted.
     """
     insert_addresses = []
-    for uuid in to_insert:
+    for address_type_uuid, value in insert_address_map.items():
         insert_addresses.append(
             Address.from_simplified_fields(
-                value=write_address_map[uuid],
-                address_type_uuid=uuid,
+                value=value,
+                address_type_uuid=address_type_uuid,
                 from_date=date.today().isoformat(),
                 person_uuid=user_uuid,
             )
@@ -49,8 +47,7 @@ def construct_insert_addresses(
 
 
 def construct_edit_addresses(
-    to_edit: set[UUID],
-    write_address_map: dict[UUID, str],
+    edit_address_map: dict[UUID, str],
     current_addresses: list,
     user_uuid: UUID,
 ) -> list[Address]:
@@ -70,14 +67,15 @@ def construct_edit_addresses(
     }
 
     edit_addresses = []
-    for uuid in to_edit:
+    for address_type_uuid, value in edit_address_map.items():
         edit_addresses.append(
             Address.from_simplified_fields(
-                value=write_address_map[uuid],
-                address_type_uuid=uuid,
-                uuid=current_address_map[uuid].uuid,
-                from_date=current_address_map[uuid].validity.from_time,
-                to_date=current_address_map[uuid].validity.to_time,
+                value=value,
+                address_type_uuid=address_type_uuid,
+                uuid=current_address_map[address_type_uuid].uuid,
+                # We can only guarantee this data is true from today
+                from_date=date.today().isoformat(),
+                to_date=current_address_map[address_type_uuid].validity.to_time,
                 person_uuid=user_uuid,
             )
         )
@@ -154,6 +152,8 @@ async def get_itsystem_uuid(
     return itsystem_uuid
 
 
+# TODO: Remove this and break function up further
+# pylint: disable=too-many-locals
 async def ensure_ad2mosynced(
     user_uuid: UUID,
     settings: Settings,
@@ -190,64 +190,98 @@ async def ensure_ad2mosynced(
         logger.info("Unable to find ADGUID itsystem on user")
         return False
     adguid = UUID(ituser.user_key)
-
     logger = logger.bind(adguid=adguid)
+
     logger.info("Synchronizing user")
 
     mappings = await ensure_mo_address_type_uuids(
         settings.ad_mappings, dataloaders.classes_loader
     )
+    mapping_address_type_uuids = set(map(attrgetter("mo_address_type_uuid"), mappings))
 
-    address_type_uuids = list(map(attrgetter("mo_address_type_uuid"), mappings))
+    # TODO: This should probably be a setting instead, defining all the address-types
+    #       that are managed by the program. For now we assume only the configured ones
+    #       are managed, and thus no deletion will ever occur.
+    managed_address_type_uuids = mapping_address_type_uuids
 
-    current_addresses = list(
-        filter(
-            lambda address: address.address_type_uuid in address_type_uuids,
-            user.addresses,
-        )
+    # Only operate on current-addresses that are managed by us, all addresses unmanaged
+    # are left untouched by the program.
+    current_address_type_uuids = set(
+        map(attrgetter("address_type_uuid"), user.addresses)
     )
-    current_address_types = {address.address_type_uuid for address in current_addresses}
+    current_address_type_uuids = current_address_type_uuids.intersection(
+        managed_address_type_uuids
+    )
 
-    current_address_map = {
-        address.address_type_uuid: address.value for address in current_addresses
-    }
-    # Assert uniqueness on existing addresses
-    assert len(current_address_map) == len(current_addresses)
+    edit_address_type_uuids = current_address_type_uuids.intersection(
+        mapping_address_type_uuids
+    )
+    insert_address_type_uuids = mapping_address_type_uuids.difference(
+        current_address_type_uuids
+    )
+    delete_address_type_uuids = current_address_type_uuids.difference(
+        managed_address_type_uuids
+    )
 
+    logger.debug("Might edit", address_type_uuids=edit_address_type_uuids)
+    logger.debug("Gonna insert", address_type_uuids=insert_address_type_uuids)
+    logger.debug("Gonna delete", address_type_uuids=delete_address_type_uuids)
+
+    # TODO: This is true until managed_address_type_uuids is set from settings.
+    assert delete_address_type_uuids == set()
+
+    current_address_tuples = [
+        (address.address_type_uuid, address.value)
+        for address in user.addresses
+        if address.address_type_uuid in current_address_type_uuids
+    ]
+    current_address_map = dict(current_address_tuples)
+    # Assert address_type uniqueness on managed existing addresses
+    if len(current_address_tuples) != len(current_address_map):
+        message = "Non uniqueness on managed existing addresses found"
+        # TODO: Include the non-unique address types
+        logger.warn(message)
+        raise ValueError(message)
+
+    # Fetch AD fields and construct write map
     result = await dataloaders.adattribute_loader.load(adguid)
     # print(result)
-
     write_address_map = {
         mapping.mo_address_type_uuid: result[mapping.ad_field] for mapping in mappings
     }
 
-    print(current_address_map)
-    print(write_address_map)
+    # address_maps are mappings from address-type to address value
+    # current is for the current addresses in MO
+    # write is for the desired addresses in MO (i.e. from the mapping)
 
-    write_address_types = set(address_type_uuids)
+    # print(current_address_map)
+    # print(write_address_map)
 
-    # TODO: Delete should be consider all 'owned' address types
-    to_delete = current_address_types.difference(write_address_types)
-    potential_to_edit = current_address_types.intersection(write_address_types)
-    to_insert = write_address_types.difference(current_address_types)
-
-    to_edit = set(
+    # We only wanna edit, if there are updates on the value
+    edit_address_type_uuids = set(
         filter(
             lambda key: current_address_map[key] != write_address_map[key],
-            potential_to_edit,
+            edit_address_type_uuids,
         )
     )
+    logger.debug("Gonna edit", address_type_uuids=edit_address_type_uuids)
 
-    print("INSERT", to_insert)
-    print("POTENTIAL_EDIT", potential_to_edit)
-    print("EDIT", to_edit)
-    print("DELETE", to_delete)
+    # Filter write_address_map by insert_address_type_uuids
+    insert_address_map = {
+        key: value
+        for key, value in write_address_map.items()
+        if key in insert_address_type_uuids
+    }
+    insert_addresses = construct_insert_addresses(insert_address_map, user_uuid)
 
-    insert_addresses = construct_insert_addresses(
-        to_insert, write_address_map, user_uuid
-    )
+    # Filter write_address_map by edit_address_type_uuids
+    edit_address_map = {
+        key: value
+        for key, value in write_address_map.items()
+        if key in edit_address_type_uuids
+    }
     edit_addresses = construct_edit_addresses(
-        to_edit, write_address_map, current_addresses, user_uuid
+        edit_address_map, user.addresses, user_uuid
     )
     # TODO: Handle DELETE
 
